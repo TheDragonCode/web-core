8b93a27a3f434e852f73b8a3da4bb30c8f18414c
Andrey Helldar <helldar@ai-rus.com>
2022-03-28 18:50:05 +0300

    Fix console config loading

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 namespace DragonCode\WebCore\Console;
 
+use DragonCode\WebCore\Foundation\Bootstrap\LoadConfiguration;
+use Illuminate\Foundation\Bootstrap\BootProviders;
+use Illuminate\Foundation\Bootstrap\HandleExceptions;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use Illuminate\Foundation\Bootstrap\RegisterFacades;
+use Illuminate\Foundation\Bootstrap\RegisterProviders;
+use Illuminate\Foundation\Bootstrap\SetRequestForConsole;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 abstract class Kernel extends ConsoleKernel
 {
+    protected $bootstrappers = [
+        LoadEnvironmentVariables::class,
+        LoadConfiguration::class,
+        HandleExceptions::class,
+        RegisterFacades::class,
+        SetRequestForConsole::class,
+        RegisterProviders::class,
+        BootProviders::class,
+    ];
+
     protected function commands()
     {
         $this->load(app_path('Console/Commands'));
