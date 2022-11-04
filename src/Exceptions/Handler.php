957d9e14b034a065fef8384336b1513c380d0f2e
Andrey Helldar <helldar@dragon-code.pro>
2022-11-04 14:19:51 +0300

    Added the ability to change the text of messages in errors

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace DragonCode\WebCore\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 abstract class Handler extends ExceptionHandler
 {
@@ -13,4 +15,24 @@ abstract class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    protected array $exceptionMapMessages = [
+        NotFoundHttpException::class => 'http-statuses.404',
+    ];
+
+    protected function convertExceptionToArray(Throwable $e): array
+    {
+        return config('app.debug')
+            ? parent::convertExceptionToArray($e)
+            : ['message' => $this->mapExceptionMessage($e)];
+    }
+
+    protected function mapExceptionMessage(Throwable $e): string
+    {
+        if ($message = $this->exceptionMapMessages[get_class($e)] ?? false) {
+            return __($message);
+        }
+
+        return $this->isHttpException($e) ? $e->getMessage() : __('http-statuses.500');
+    }
 }
